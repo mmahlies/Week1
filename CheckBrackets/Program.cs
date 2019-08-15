d424326156576f7388b9818cd1f5baa5cfe1520a
mmahlies <eng.m.mahlies@gmail.com>
2019-08-15 22:49:21 +0200

    Add project files.

---
+++ CheckBrackets/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringInput = Console.ReadLine();
+            int errorKey = CheckBracketHasError(stringInput);
+            if (errorKey != -1)
+            {
+                Console.WriteLine(errorKey + 1);
+            }
+            else
+            {
+                Console.WriteLine("Success");
+            }
+            
+        }
+        /// <summary>
+        /// for every char has tow cases
+        /// char
+        /// opend open 
+        ///     push into stack
+        /// Closed one
+        ///     if stack empty then error
+        ///     if stsck.peek .has closed then error
+        ///     if stack.peek has open with type diffrent from char then error
+        ///     if stack.peek has open with type same char then continue 
+        /// @ the end stack must be empty
+        /// </summary>
+        /// <param name="stringInput"></param>
+        /// <returns></returns>
+        private static int CheckBracketHasError(string stringInput)
+        {
+            Stack<KeyValuePair<char, int>> stack = new Stack<KeyValuePair<char, int>>();
+
+            Dictionary<char, char> openDic = new Dictionary<char, char>();
+            Dictionary<char, char> closeDic = new Dictionary<char, char>();
+            openDic.Add('(', ')');
+            openDic.Add('[', ']');
+            openDic.Add('{', '}');
+            closeDic.Add('}', '{');
+            closeDic.Add(')', '(');
+            closeDic.Add(']', '[');
+
+
+            for (int i = 0; i < stringInput.Length; i++)
+            {
+                // closed one
+                char closeDicValue;
+                char openDicValue;
+                if (closeDic.TryGetValue(stringInput[i], out closeDicValue))
+                {
+                    if (stack.Count == 0)
+                    {
+                        return i;
+                    }
+                    KeyValuePair<char, int> temp = stack.Pop();
+                    /// open bracket                
+                    if (openDic.TryGetValue(temp.Key, out  openDicValue))
+                    {
+                        // open and the same kind as closed one 
+                        if (openDicValue == stringInput[i])
+                        {
+                            continue;
+                        }
+                        else
+                        {  // open with diffrent kind
+                            return i;
+                        }
+                    }
+                    // close bracket
+                    else
+                    {
+                        return i;
+                    }
+                }              
+                else if (openDic.TryGetValue(stringInput[i], out  openDicValue ))
+                {
+                    stack.Push(new KeyValuePair<char, int>(stringInput[i], i));
+                }
+            }
+            // ensure from 
+            if (stack.Count != 0)
+            {
+                return stack.Pop().Value;
+            }
+            return -1;
+        }
+    }
+}
---
+++ ComputeTreeHeight/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputeTreeHeight
+{
+    class Program
+    {
+        /// <summary>
+        /// The first line contains the number of nodes 𝑛. The second line contains 𝑛 integer numbers
+        //  from −1 to 𝑛 − 1 — parents of nodes.If the 𝑖-th one of them(0 ≤ 𝑖 ≤ 𝑛 − 1) is −1, node 𝑖 is the root,
+        //otherwise it’s 0-based index of the parent of 𝑖-th node.It is guaranteed that there is exactly one root.
+        //It is guaranteed that the input represents a tree.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arrayInput = Console.ReadLine().Split(' ').Select(w => int.Parse(w)).ToArray();
+            BST bST = new BST(arrayInput);
+
+            int height = bST.GetHeight();
+            Console.WriteLine(height);
+        }
+    }
+    class Node
+    {
+        int value;
+        List<Node> children = new List<Node>();
+        public void AddChildren(int valuePr)
+        {
+            this.children.Add(new Node(valuePr));
+        }
+        public void AddChildren(Node nodeValue)
+        {
+            this.children.Add((nodeValue));
+        }
+        public Node(int valuePr)
+        {
+            this.value = valuePr;
+        }
+        public List<Node> GetChildren()
+        {
+            return this.children;
+        }
+    }
+
+    class BST
+    {
+        Node root;
+        Node[] nodes;
+        public BST(int[] nodeArray)
+        {
+            nodes = new Node[nodeArray.Length];
+            for (int child = 0; child < nodeArray.Length; child++)
+            {
+                int parent = nodeArray[child];
+
+                if (nodes[child] == null)
+                    nodes[child] = new Node(child);
+
+                if (parent == -1)
+                {
+                    this.root = nodes[child];
+                    nodes[child] = root;
+                }
+                else
+                {
+                    if (nodes[parent] == null)
+                        nodes[parent] = new Node(parent);
+
+                    nodes[parent].AddChildren(nodes[child]);
+                }
+            }
+        }
+        public int GetHeight()
+        {
+            Queue<Node> queue = new Queue<Node>();
+            int height = 0;
+            int nodeCount = 0;
+            queue.Enqueue(this.root);
+            while (true)
+            {
+                nodeCount = queue.Count;
+                if (nodeCount == 0)
+                {
+                    break;
+                }
+                height++;
+                while (nodeCount > 0)
+                {
+                    Node TempNode = queue.Dequeue();
+                    List<Node> childrenTemp = TempNode.GetChildren();
+                    foreach (Node child in childrenTemp)
+                    {
+                        queue.Enqueue(child);
+                    }
+                    nodeCount--;
+                }
+            }
+            return height;
+        }
+    }
+}
